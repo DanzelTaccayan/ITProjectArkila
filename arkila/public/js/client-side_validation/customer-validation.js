86ca07d4519b7b3aba7d662cc714271c59848b09
VLORIA2152015 <2152015@slu.edu.ph>
2018-04-06 22:20:35 +0800

    added validation to customer registration

---
+++ arkila/public/js/client-side_validation/customer-validation.js
@@ -0,0 +1,30 @@
+$('[val-name]').parsley({
+      maxlength: 30,
+      pattern: /^[a-zA-Z]$|^[a-zA-Z][a-zA-Z\s-]*[a-zA-Z]$/,
+      minlength: 2
+ });	
+
+$('[name="first_name"]').attr('data-parsley-required-message','Please enter a first name.');
+$('[name="last_name"]').attr('data-parsley-required-message','Please  enter a last name.');
+$('[val-name]').attr('data-parsley-pattern-message','Please use letters (a-z) only.');
+
+$('[val-username]').parsley({
+  maxlength: 12,
+  pattern: /^[\dA-Za-z][A-Za-z\d.-]*[A-Za-z\d]$/,
+  minlength: 6
+});
+
+$('[name="username"]').attr('data-parsley-required-message','Please enter a username');
+$('[val-username]').attr('data-parsley-pattern-message','Username should only contain letters, numbers, periods, and hyphen');
+
+
+$('[name="email"]').attr('data-parsley-required-message','Please enter an email');
+
+$('[val-password]').parsley({
+  maxlength: 15,
+  minlength: 8
+});
+
+$('[val-password]').attr('data-parsley-required-message','Password is required');
+$('[name="password_confirmation"]').attr('data-parsley-equalto','#customerPassword');
+$('[name="password_confirmation"]').attr('data-parsley-equalto-message','Password does not match');
