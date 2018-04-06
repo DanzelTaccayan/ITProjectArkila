5c4532f7e95da35385b91bc657d957ea530072c2
Danzel Taccayan <taccayandanzel@gmail.com>
2018-04-06 23:49:57 +0800

    edited customer module -chabalto

--- arkila/resources/views/customermodule/non-user/sign-up/signUp.blade.php
+++ arkila/resources/views/customermodule/non-user/sign-up/signUp.blade.php
@@ -5,39 +5,35 @@
             <div class="heading text-center">
                 <h2>Sign Up</h2>
             </div>
-            <div class="row">
-                <div class="col-md-4"></div>
-                <div class="col-md-4" id="boxContainer">
-                    <form class="contact100-form" action="{{route('register')}}" method="POST" data-parsley-validate="">
-                        {{csrf_field()}}
-                        <div class="wrap-input100" >
-                            <input id="customerName" class="input100" type="text" name="name" value="{{ old('name') }}" placeholder="Full Name"  val-name required>
-                        </div><!-- wrap-input100-->
-                        <div class="wrap-input100">
-                            <input id="customerUsername" class="input100" type="text" name="username" value="{{ old('username') }}" placeholder="Username" val-name required>
-                            
-                        </div><!-- wrap-input100-->
-                        <div class="wrap-input100">
-                            <input id="customerEmail" class="input100" type="text" name="email" value="{{ old('email') }}" placeholder="Email Address">
-                              
-                        </div><!-- wrap-input100-->
-                        <div class="wrap-input100">
-                            <input id="customerPassword" class="input100" type="password" name="password" placeholder="Password" required>
-                            
-                        </div><!-- wrap-input100-->
-                        <div class="wrap-input100">
-                            <input id="customerRepeatPassword" class="input100" type="password" name="password_confirmation" placeholder="Repeat Password" required>
-                            <span class="focus-input100"></span>
-                        </div><!-- wrap-input100-->
-                        <div class="container-contact100-form-btn">
-                            <button type="submit" class="contact100-form-btn"><strong>Sign Up</strong></button>
-                        </div><!-- container-contact100-form-btn-->
-                    </form>
-                    <!-- contact100-form-->
-                </div>
-                <!-- boxContainer-->
+            <div class="col-md-4 mx-auto" id="boxContainer">
+                <form class="contact100-form" action="{{route('register')}}" method="POST" data-parsley-validate="">
+                    {{csrf_field()}}
+                    <div class="wrap-input100" >
+                        <input id="customerName" class="input100" type="text" name="name" value="{{ old('name') }}" placeholder="Full Name"  val-name required>
+                    </div><!-- wrap-input100-->
+                    <div class="wrap-input100">
+                        <input id="customerUsername" class="input100" type="text" name="username" value="{{ old('username') }}" placeholder="Username" val-name required>
+
+                    </div><!-- wrap-input100-->
+                    <div class="wrap-input100">
+                        <input id="customerEmail" class="input100" type="text" name="email" value="{{ old('email') }}" placeholder="Email Address">
+
+                    </div><!-- wrap-input100-->
+                    <div class="wrap-input100">
+                        <input id="customerPassword" class="input100" type="password" name="password" placeholder="Password" required>
+
+                    </div><!-- wrap-input100-->
+                    <div class="wrap-input100">
+                        <input id="customerRepeatPassword" class="input100" type="password" name="password_confirmation" placeholder="Repeat Password" required>
+                        <span class="focus-input100"></span>
+                    </div><!-- wrap-input100-->
+                    <div class="container-contact100-form-btn">
+                        <button type="submit" class="contact100-form-btn"><strong>Sign Up</strong></button>
+                    </div><!-- container-contact100-form-btn-->
+                </form>
+                <!-- contact100-form-->
             </div>
-            <!-- row-->
+            <!-- boxContainer-->
         </div>
         <!-- container-->
     </section>
