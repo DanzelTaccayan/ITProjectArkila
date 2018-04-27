53231075d42c50ad37c6555ff457dba35a9eae3e
teodorodelson <2150948@slu.edu.ph>
2018-04-27 18:26:05 +0800

    On the way to fix van queue

--- arkila/app/Http/Controllers/TripsController.php
+++ arkila/app/Http/Controllers/TripsController.php
@@ -16,93 +16,14 @@ use Illuminate\Validation\Rule;
 
 class TripsController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        $terminals = Terminal::whereNotIn('terminal_id',[auth()->user()->terminal_id])->get();
-        $trips = Trip::whereNotNull('queue_number')->orderBy('queue_number')->get();
-
-        $drivers = Member::whereNotIn('member_id', function($query){
-            $query->select('driver_id')->from('trip')->whereNotNull('queue_number');
-        })->where('status','Active')->get();
-
-        $vans = Van::whereNotIn('plate_number', function($query){
-            $query->select('plate_number')
-                ->from('trip')
-                ->where('has_privilege',1)
-                ->orWhereNotNull('queue_number');
-        })->where('status','Active')->get();
-
-
-        return view('trips.queue', compact('terminals','trips','vans','destinations','drivers'));
-    }
-
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Terminal $destination, Van $van, Member $member )
-    {
-        if( is_null(Trip::where('terminal_id',$destination->terminal_id)
-            ->where('plate_number',$van->plate_number)
-            ->whereNotNull('queue_number')->first()) ){
-            $queueNumber = Trip::where('terminal_id',$destination->terminal_id)
-                ->whereNotNull('queue_number')
-                    ->count()+1;
-
-            Trip::create([
-                'terminal_id' => $destination->terminal_id,
-                'plate_number' => $van->plate_number,
-                'driver_id' => $member->member_id,
-                'remarks' => NULL,
-                'queue_number' => $queueNumber
-            ]);
-            session()->flash('success', 'Van Succesfully Added to the queue');
-            return 'success';
-        }
-        else{
-            session()->flash('error', 'Van is already on the Queue');
-            return 'Van is already on the Queue';
-        }
-
-    }
 
 
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function updateRemarks(Trip $trip)
-    {
-
-        $this->validate(request(),[
-            'value' => [Rule::in('OB','CC','ER', 'NULL')]
-        ]);
 
 
-        if(request('value') === 'NULL'){
-            $trip->update([
-                'remarks' => NULL
-            ]);
-        }else{
-            $trip->update([
-                'remarks' => request('value')
-            ]);
-        }
 
-        return 'success';
-    }
+
+
 
     public function updateDestination(Trip $trip)
     {
---
+++ arkila/resources/views/van_queue/viewTrip.blade.php
@@ -0,0 +1,110 @@
+@extends('layouts.master') 
+@section('title', 'Trip Details')
+@section('links')
+@section('content')
+
+<div class="box" style="box-shadow: 0px 5px 10px gray;">
+    <div class="box-header with-border text-center">
+        <a href="{{route('trips.tripLog')}}" class="pull-left btn btn-default"><i class="fa  fa-chevron-left"></i></a>
+        <h3 class="box-title">
+            Trip Details
+        </h3>
+    </div>
+    <div class="box-body">
+        <div class="box" >
+            <div class="box-body">
+                <div class="row">
+                    <div class="col-lg-6">
+                        <div style="width:70%">
+                            <div class="header text-center">
+                                <h4>Destination</h4>
+                            </div>
+                            <div class="box-body" id="inner-dest">
+
+                                @php $totalPassengers = 0; @endphp
+                                @foreach($destinations as $key => $values)
+                                    @if($trip->trip_id == $values->tripid)
+                                    @php $innerRoutesArr[$key] = $values; @endphp
+                                <div class="form-group inner-routes">
+                                    <label for="">{{$values->destdesc}}</label>
+                                    <input class="form-control pull-right" type="number" id="qty{{$trip->trip_id}}" style="width:30%;" value="{{$values->counts}}" disabled>
+                                </div>
+                                    @php $totalPassengers = $totalPassengers + $values->counts; @endphp
+                                    @endif
+                                @endforeach
+                                
+
+                                <label for="">Total</label>
+                                <input id="" class="form-control pull-right" type="text" id="total" style="width:30%;" value="{{$totalPassengers}}" disabled>
+                            </div>
+                        </div>
+
+                    </div>
+
+                    <div class="col-lg-6">
+                        <div style="width:80%">
+                        <div class="header text-center">
+                            <h4>Summary</h4>
+                        </div>
+                         <div>
+                            <label>Driver:</label>
+                            <name>{{$trip->driver->first_name . " " . $trip->driver->middle_name . " " . $trip->driver->last_name}}</name>
+                        </div>
+                        <div>
+                            <label>Van:</label>
+                            <name>{{$trip->plate_number}}</name>
+                        </div> 
+                        <div>
+                            <label>Origin:</label>
+                            <name>{{$superAdmin->description}}</name>
+                        </div>
+                        <div>
+                            <label>Destination:</label>
+                            <name>{{$trip->terminal->description}}</name>
+                        </div>
+                        <div>
+                            <label>Date:</label>
+                            <name>{{$trip->date_departed}}</name>
+                        </div>
+                        <div>
+                            <label>Time:</label>
+                            <name>{{$trip->time_departed}}</name>
+                        </div> 
+                        
+                        <div class="box" style="margin: 3% 0%">
+                            <div class="box-header text-center">
+                                <h4>Shares</h4>
+                            </div>
+                            <div class="box-body" id="inner-dest">
+                                <div class="form-group inner-routes">
+                                    @php $bantrans = 0; @endphp
+                                    @if($trip->SOP == null)
+                                        @php $bantrans = $trip->total_booking_fee + $trip->community_fund  @endphp
+                                    @else
+                                        @php $bantrans = $trip->total_booking_fee + $trip->SOP + $trip->community_fund  @endphp
+                                    @endif
+                                    
+                                    @php $totalfare = 0; @endphp
+                                    @foreach($destinations as $key => $values)
+                                        @php $totalfare = $totalfare + ($values->amount * $values->counts); @endphp
+                                    @endforeach
+                                   
+                                </div>
+                                
+                                <label for="">Driver:</label>
+                                <input id="" class="form-control pull-right" type="number" id="total" style="width:30%;" value="{{$totalfare - $bantrans}}" disabled>
+                                
+                            </div>
+                        </div>
+                        
+                        <button onclick="window.open('{{route('pdf.perTrip', $trip->trip_id)}}')" class="btn btn-default btn-sm btn-flat pull-right"> <i class="fa fa-print"></i> PRINT</button>
+
+                    </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
