ed85afa80ac19ae8d3a2c4a4d707ff0fe5892681
VLORIA2152015 <2152015@slu.edu.ph>
2018-05-12 20:17:45 +0800

    added questions in FAQ customer module

--- arkila/resources/views/customermodule/user/help/customerHelp.blade.php
+++ arkila/resources/views/customermodule/user/help/customerHelp.blade.php
@@ -5,6 +5,74 @@
             <div class="heading text-center">
                 <h2>FAQ's</h2>
             </div>
+            <div class="box box-solid">
+                <div class="box-body">
+                    <h4>YOUR ACCOUNT</h4>
+                    <div class="question-list">  
+                        <div class="question-item">  
+                            <p><strong>Why should I sign up?</strong></p>
+                            <p>ANSWER</p>
+                        </div>
+                        <div class="question-item">  
+                            <p><strong>Can I request for a rental/reservation without signing in?</strong></p>
+                            <p>ANSWER</p>
+                        </div>
+                        <div class="question-item">  
+                            <p><strong>How do I sign up?</strong></p>
+                            <p>ANSWER</p>
+                        </div>
+                        <div class="question-item">  
+                            <p><strong>How do I recover my password?</strong></p>
+                            <p>ANSWER</p>
+                        </div>
+                    </div>
+                    <h4>RESERVATION/RENTAL</h4>
+                    <div class="question-list">
+                        <div class="question-item">  
+                            <p><strong>How to rent a van online?</strong></p>
+                            <p>Step (1)</p>
+                            <p>Step (2)</p>
+                        </div>
+                        <div class="question-item">  
+                            <p><strong>Is there a walk-in rental booking?</strong></p>
+                            <p>ANSWER</p>
+                        </div>
+                        <div class="question-item">  
+                            <p><strong>How to reserve a line?</strong></p>
+                            <p>ANSWER</p>
+                        </div>
+                        <div class="question-item">  
+                            <p><strong>How to view your rentals/reservations</strong></p>
+                            <p>ANSWER</p>
+                        </div>
+                        <div class="question-item">  
+                            <p><strong>Can I refund?</strong></p>
+                            <p>ANSWER</p>
+                        </div>
+                        <div class="question-item">  
+                            <p><strong>How do I reach you?</strong></p>
+                            <p>ANSWER</p>
+                        </div>
+                    </div>
+                    <h4>PAYMENTS</h4>
+                    <div class="question-list"> 
+                        <div class="question-item">  
+                            <p><strong>How can I pay?</strong></p>
+                            <p>ANSWER</p>
+                        </div>
+                    </div>
+                    <h4>JOB</h4>
+                    <div class="question-list"> 
+                        <div class="question-item">
+                            <p><strong>I want to be a driver. Can I apply for the job?</strong></p>
+                            <p></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="heading text-center">
+                <h2>FAQ's</h2>
+            </div>
             <div class="card card-primary">
                 <div class="card-header">
                     <h5>1. How to rent a van?</h5>
