7b6c52cb569ebef1a229280fbd2e6525721e9f2c
Juan Miguel Delos Santos <2153820@slu.edu.ph>
2018-05-18 18:14:11 +0800

    remove unecessary file in settings

--- arkila/resources/views/trips/viewTrip.blade.php
+++ arkila/resources/views/trips/viewTrip.blade.php
@@ -27,47 +27,6 @@
     </div>
     <div class="box" style="box-shadow: 0px 5px 10px gray;">
         <div class="row">
-            <div class="col-md-6" style="padding: 2% 4%">
-
-                <div class="text-center">
-                    <h4 class="report-header sblue">PASSENGER COUNT</h4>
-                </div>
-
-                <div class="box-body" id="inner-dest">
-
-                    <table class="table table-bordered table-striped table-responsive">
-                        <tbody>
-                            <tr>
-                                <th class="text-center">Route</th>
-                                <th class="text-center">Regular</th>
-                                <th class="text-center">Discounted</th>
-                            </tr>
-                            @php $totalArr = null; @endphp @foreach($tempArr as $key => $values)
-                            <tr>
-                                <td class="text-center">{{$key}}</td>
-                                @foreach($values as $innerKeys => $innerValues)
-                                <td class="text-right">{{$innerValues}}</td>
-                                @endforeach
-                            </tr>
-                            @endforeach
-                        </tbody>
-
-                        @php $totalcount = array(); @endphp @foreach($tempArr as $key => $values) @foreach($values as $innerKeys => $innerValues) @if(!array_key_exists($innerKeys, $totalcount)) @php $totalcount[$innerKeys] = 0; @endphp @endif @php $totalcount[$innerKeys] += $innerValues; @endphp @endforeach @endforeach
-
-                        <tfoot>
-                            <tr>
-                                <th class="text-right">Total Passenger</th>
-                                @foreach($totalcount as $key => $values)
-                                <th class="text-right">{{$values}}</th>
-                                @endforeach
-                            </tr>
-
-                        </tfoot>
-                    </table>
-                </div>
-
-            </div>
-
             <div class="col-md-6" style="padding: 2% 5%">
                 <div class="text-center">
                     <h4 class="report-header msgreen">DEPARTURE DETAILS</h4>
@@ -120,8 +79,49 @@
                         </tr>
                     </tbody>
                 </table>
-
+                
                 <button onclick="window.open('')" class="btn btn-primary btn-sm btn-flat pull-right"> <i class="fa fa-print"></i> PRINT TRIP INFORMATION</button>
+
+            </div>
+
+            <div class="col-md-6" style="padding: 2% 4%">
+
+                <div class="text-center">
+                    <h4 class="report-header sblue">PASSENGER COUNT</h4>
+                </div>
+
+                <div class="box-body" id="inner-dest">
+
+                    <table class="table table-bordered table-striped table-responsive">
+                        <tbody>
+                            <tr>
+                                <th class="text-center">Route</th>
+                                <th class="text-center">Regular</th>
+                                <th class="text-center">Discounted</th>
+                            </tr>
+                            @php $totalArr = null; @endphp @foreach($tempArr as $key => $values)
+                            <tr>
+                                <td class="text-center">{{$key}}</td>
+                                @foreach($values as $innerKeys => $innerValues)
+                                <td class="text-right">{{$innerValues}}</td>
+                                @endforeach
+                            </tr>
+                            @endforeach
+                        </tbody>
+
+                        @php $totalcount = array(); @endphp @foreach($tempArr as $key => $values) @foreach($values as $innerKeys => $innerValues) @if(!array_key_exists($innerKeys, $totalcount)) @php $totalcount[$innerKeys] = 0; @endphp @endif @php $totalcount[$innerKeys] += $innerValues; @endphp @endforeach @endforeach
+
+                        <tfoot>
+                            <tr>
+                                <th class="text-center">Total Passenger</th>
+                                @foreach($totalcount as $key => $values)
+                                <th class="text-right">{{$values}}</th>
+                                @endforeach
+                            </tr>
+
+                        </tfoot>
+                    </table>
+                </div>
             </div>
         </div>
     </div>
