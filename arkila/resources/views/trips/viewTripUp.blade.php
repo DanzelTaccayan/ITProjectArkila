254f79dee68c48989b89da95de9070edf8b48629
Juan Miguel Delos Santos <2153820@slu.edu.ph>
2018-05-18 13:59:42 +0800

    fixed the ui of view trip

--- arkila/resources/views/trips/viewTripUp.blade.php
+++ arkila/resources/views/trips/viewTripUp.blade.php
@@ -1,109 +1,119 @@
 @extends('layouts.master')
 @section('title', 'Trip Details')
 @section('links')
+@section('links')
+@parent
+<style>
+.report-header {
+    padding: 10px;
+    color: white;
+}
+.sblue {
+    background: slateblue;
+}
+
+.msgreen {
+    background: mediumseagreen;
+}
+</style>
+@endsection
 @section('content')
+<div class="padding-side-5">
+    <div>
+        <h2 class="text-white">VIEW TRIP</h2>
+    </div>
 
 <div class="box" style="box-shadow: 0px 5px 10px gray;">
-    <div class="box-header with-border text-center">
-        <a href="{{route('trips.tripLog')}}" class="pull-left btn btn-default"><i class="fa  fa-chevron-left"></i></a>
-        <h3 class="box-title">
-            Trip Details
-        </h3>
-    </div>
-    <div class="box-body">
-        <div class="box" >
-            <div class="box-body">
-                <div class="row">
-                    <div class="col-lg-6">
-                        <div style="width:70%">
-                            <div class="header text-center">
-                                <h4>Destination</h4>
-                            </div>
-                            <div class="box-body" id="inner-dest">
-                              <table class="table table-bordered table-striped table-responsive">
-                                <thead>
-                                  <th></th>
-                                  <th>#Passenger</th>
-                                  <th>#Discounted</th>
-                                </thead>
-                                <tbody>
-                                  <tr>
-                                    <th>Main Terminal</th>
-                                    <td>{{$numPassCountArr['mainTerminalRegular']}}</td>
-                                    <td>{{$numPassCountArr['mainTerminalDiscount']}}</td>
-                                  </tr>
-                                  <tr>
-                                    <th>Short Trip</th>
-                                    <td>{{$numPassCountArr['shortTripRegular']}}</td>
-                                    <td>{{$numPassCountArr['shortTripDiscount']}}</td>
-                                  </tr>
-                                </tbody>
-                                <tfoot>
-                                  <tr>
-                                    <th class="text-right">Total</th>
-                                    <th class="text-right">{{$numPassCountArr['mainTerminalRegular'] + $numPassCountArr['shortTripRegular']}}</th>
-                                    <th class="text-right">{{$numPassCountArr['mainTerminalDiscount'] + $numPassCountArr['shortTripDiscount']}}</th>
-                                  </tr>
-                                </tfoot>
-                              </table>
-                            </div>
-                        </div>
-
-                    </div>
-
-                    <div class="col-lg-6">
-                      <div style="width:80%">
-                      <div class="header text-center">
-                          <h4>Summary</h4>
-                      </div>
-                       <div>
-                          <label>Driver:</label>
-                          <name>{{$trip->driver->first_name . ' ' . $trip->driver->last_name}}</name>
-                      </div>
-                      <div>
-                          <label>Van:</label>
-                          <name>{{$trip->van->plate_number}}</name>
-                      </div>
-                      <div>
-                          <label>Origin:</label>
-                          <name>{{$trip->origin}}</name>
-                      </div>
-                      <div>
-                          <label>Destination:</label>
-                          <name>{{$trip->destination}}</name>
-                      </div>
-                      <div>
-                          <label>Date:</label>
-                          <name>{{$trip->date_departed}}</name>
-                      </div>
-                      <div>
-                          <label>Time:</label>
-                          <name>{{$trip->time_departed}}</name>
-                      </div>
-
-                        <div class="box" style="margin: 3% 0%">
-                            <div class="box-header text-center">
-                                <h4>Shares</h4>
-                            </div>
-                            <div class="box-body" id="inner-dest">
-                                <div class="form-group inner-routes">
-
-                                </div>
-
-                                <label for="">Driver:</label>
-                                <input id="" class="form-control pull-right" type="number" id="total" style="width:30%;" value="{{number_format((float)$driverShare, 2, '.', '')}}" disabled>
-
-                            </div>
-                        </div>
-
-
-
-                    </div>
-                    </div>
+    <div class="row">
+        <div class="col-md-6" style="padding: 2% 4%">
+            
+                <div class="text-center"><h4 class="report-header sblue">PASSENGER COUNT</h4></div>
+                <div class="box-body" id="inner-dest">
+                    <table class="table table-bordered table-striped table-responsive">
+                        <thead>
+                            <th></th>
+                            <th>#Passenger</th>
+                            <th>#Discounted</th>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <th>Main Terminal</th>
+                                <td class="text-right">{{$numPassCountArr['mainTerminalRegular']}}</td>
+                                <td class="text-right">{{$numPassCountArr['mainTerminalDiscount']}}</td>
+                            </tr>
+                            <tr>
+                                <th>Short Trip</th>
+                                <td class="text-right">{{$numPassCountArr['shortTripRegular']}}</td>
+                                <td class="text-right">{{$numPassCountArr['shortTripDiscount']}}</td>
+                            </tr>
+                        </tbody>
+                        <tfoot>
+                            <tr>
+                                <th class="text-right">Total</th>
+                                <th class="text-right">{{$numPassCountArr['mainTerminalRegular'] + $numPassCountArr['shortTripRegular']}}</th>
+                                <th class="text-right">{{$numPassCountArr['mainTerminalDiscount'] + $numPassCountArr['shortTripDiscount']}}</th>
+                            </tr>
+                        </tfoot>
+                    </table>
                 </div>
+            
+        </div>
 
-            </div>
+        <div class="col-md-6" style="padding: 2% 5%">
+      
+                <div class="text-center"><h4 class="report-header msgreen">DEPARTURE DETAILS</h4></div>
+                
+                      <table class="table">
+                        <tbody>
+                            <tr>
+                                <th>Driver:</th>
+                                <td>{{$trip->driver->first_name . ' ' . $trip->driver->last_name}}</td> 
+                            </tr>
+                            <tr>
+                                <th>Van:</th>
+                                <td>{{$trip->van->plate_number}}</td>       
+                            </tr>
+                            <tr>
+                                <th>Origin</th>
+                                <td>{{$trip->origin}}</td>  
+                            </tr>
+                            <tr>
+                                <th>Destination</th>
+                                <td>{{$trip->destination}}</td>  
+                            </tr>
+                            <tr>
+                                <th>Date:</th>
+                                <td>{{$trip->date_departed}}</td>  
+                            </tr>
+                            <tr>
+                                <th>Time:</th>
+                                <td>{{$trip->time_departed}}</td>  
+                            </tr>
+                        </tbody>
+                    </table>
+
+                <div class="text-center"><h4 class="report-header msgreen">SHARES</h4></div>
+                <table class="table table-bordered table-striped table-responsive">
+                        <tbody>
+                            <tr>
+                                <td>Total</td>
+                                <td class="text-right"></td>   
+                            </tr>
+                            <tr>
+                                <td>Office</td>
+                                <td class="text-right"></td>       
+                            </tr>
+                            <tr>
+                                <th>Driver</th>
+                                <th class="text-right">{{number_format((float)$driverShare, 2, '.', '')}}</th>  
+                            </tr>
+                        </tbody>
+                    </table>
+
+                    <button onclick="window.open('')"  class="btn btn-primary btn-sm btn-flat pull-right"> <i class="fa fa-print"></i> PRINT TRIP INFORMATION</button>
+            
         </div>
     </div>
+</div>
 </div>
 @endsection
