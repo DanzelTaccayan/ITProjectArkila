b9c0c8a3f79eb5f80cd3ae4fb0c7fe460469295e
Marvie Yuki <marvieyuki06@gmail.com>
2018-05-19 21:27:42 +0800

    reservation_date table migration

---
+++ arkila/database/migrations/2018_05_19_211943_create_reservation_date_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateReservationDateTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reservation_date', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('destination_terminal')
+            ->unsigned();
+            $table->date('reservation_date');
+            $table->integer('number_of_slots');
+            $table->timestamps();
+
+            $table->foreign('destination_terminal')
+            ->references('destination_id')->on('destination')
+            ->onDelete('cascade')
+            ->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reservation_date');
+    }
+}
