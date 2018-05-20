46d6be47e380e64678232930d7f7822af437a0d6
Juan Miguel Delos Santos <2153820@slu.edu.ph>
2018-05-20 18:59:21 +0800

    added operator archive interface

--- arkila/app/Http/Controllers/TestController.php
+++ arkila/app/Http/Controllers/TestController.php
@@ -13,7 +13,7 @@ class TestController extends Controller
      */
     public function index()
     {
-        return view('ticketmanagement.queue');
+        return view('archive.operatorArchive');
     }
 
     /**
