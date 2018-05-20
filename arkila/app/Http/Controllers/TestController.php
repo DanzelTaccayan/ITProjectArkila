9b308d07c06b446692e7e5d57ebcbb9122a09ad6
Juan Miguel Delos Santos <2153820@slu.edu.ph>
2018-05-20 19:34:21 +0800

    added image in company profile

--- arkila/app/Http/Controllers/TestController.php
+++ arkila/app/Http/Controllers/TestController.php
@@ -13,7 +13,7 @@ class TestController extends Controller
      */
     public function index()
     {
-        return view('archive.operatorArchive');
+        return view('ticketmanagement.queue');
     }
 
     /**
