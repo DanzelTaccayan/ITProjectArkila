4ac05b64117f1acc19f9a39b1ee86e181cfce486
VLORIA2152015 <2152015@slu.edu.ph>
2018-05-22 18:10:48 +0800

    modified the structure of landing page of non user and customer

---
+++ arkila/resources/views/layouts/partials/customer_carousel.blade.php
@@ -0,0 +1,68 @@
+<section style="background: url('{{ URL::asset('img/photogrid.jpg') }}') center center repeat; background-size: cover;" class="bar background-white relative-positioned">
+        <div class="container">
+            <!-- Carousel Start-->
+            <div class="home-carousel">
+                <div class="dark-mask mask-primary"></div>
+                <div class="container">
+                    <div class="homepage owl-carousel">
+                        <div class="item">
+                            <div class="row">
+                                <div class="col-md-5 text-right">
+                                    <h1>3 Branches to serve you!</h1>
+                                    <p>Baguio. Cabanatuan. San Jose.</p>
+                                </div>
+                                <div class="col-md-7"><img src="{{ URL::asset('img/template-homepage.png') }}" alt="" class="img-fluid">
+                                </div>
+                                <!-- col-->
+                            </div>
+                            <!-- row-->
+                        </div>
+                        <!-- item-->
+                        <div class="item">
+                            <div class="row">
+                                <div class="col-md-7 text-center"><img src="../img/template-mac.png" alt="" class="img-fluid"></div>
+                                <div class="col-md-5">
+                                    <h2>Baguio Terminal Operating Hours</h2>
+                                    <ul class="list-unstyled">
+                                        <li>Operating Hours 4:00 am - 6:00 pm daily</li>
+                                        <li>Office Hours 7:00 am - 7:00 pm daily</li>
+                                    </ul>
+                                </div>
+                                <!-- col-->
+                            </div>
+                            <!-- row-->
+                        </div>
+                        <!-- item-->
+                        <div class="item">
+                            <div class="row">
+                                <div class="col-md-5 text-right">
+                                    <h1>Need a Van Rental for a special occasion?</h1>
+                                    <p>Ban Trans is here for you!</p>
+                                </div>
+                                <div class="col-md-7"><img src="../img/template-easy-customize.png" alt="" class="img-fluid"></div>
+                                <!-- col-->
+                            </div>
+                            <!-- row-->
+                        </div>
+                        <!-- item-->
+                        <div class="item">
+                            <div class="row">
+                                <div class="col-md-7"><img src="../img/template-easy-code.png" alt="" class="img-fluid"></div>
+                                <div class="col-md-5">
+                                    <h1>Need a Reservation for a Trip?</h1>
+                                    <p>We got you!</p>
+                                </div>
+                                <!-- col-->
+                            </div>
+                            <!-- row-->
+                        </div>
+                        <!-- item-->
+                    </div>
+                    <!-- homepage-->
+                </div>
+                <!-- container-->
+            </div>
+            <!-- Carousel End-->
+        </div>
+        <!-- section-->
+</section>
