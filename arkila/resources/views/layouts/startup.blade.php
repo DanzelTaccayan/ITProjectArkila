de2d88c753d6cdfddc6b20f5727cb750a4595dbf
Juan Miguel Delos Santos <2153820@slu.edu.ph>
2018-05-24 11:00:02 +0800

    fixed the layout for start up and live queue

--- arkila/resources/views/layouts/startup.blade.php
+++ arkila/resources/views/layouts/startup.blade.php
@@ -13,55 +13,22 @@
 </head>
 
 <!-- ADD THE CLASS layout-top-nav TO REMOVE THE SIDEBAR. -->
-<body class="hold-transition skin-blue layout-top-nav">
-    <div class="wrapper">
-        <!-- Full Width Column -->
-        <div class="content-wrapper bgform-image">
-            <div class="container">
+<body class="hold-transition skin-blue layout-top-nav bgform-image">
+   
+    <form id="@yield('form-id')" class="parsley-form" action="@yield('form-action')" method="POST" data-parsley-validate="" enctype="multipart/form-data">
+    {{csrf_field()}}
+    @yield('method_field')
 
-                <form id="@yield('form-id')" class="parsley-form" action="@yield('form-action')" method="POST" data-parsley-validate="" enctype="multipart/form-data">
-                {{csrf_field()}}
-                @yield('method_field')
+    <section class="content" style="padding: 0% 15%">
+    @yield('form-body')
 
-                <section class="content" style="padding: 0% 15%">
-                @yield('form-body')
+    </section>
+    </form>
+        
+    @include('layouts.partials.footer')
 
-                <div class="modal fade" id="form-modal">
-                    <div class="modal-dialog modal-sm">
-                        <div class="modal-content">
-                            <div class="modal-header bg-primary">
-                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                <span aria-hidden="true">&times;</span></button>
-                                <h4 class="modal-title"> @yield('modal-title') </h4>
-                            </div>
-                            <!-- /.modal-header -->
-                            <div class="modal-body">
-                                @yield('modal-body')
-                            </div>
-                            <!-- /.modal-body -->
-                            <div class="modal-footer">
-                                @yield('modal-btn')
-                            </div>
-                            <!--/.modal-foorer -->
-                        </div>
-                        <!-- /.modal-content -->
-                    </div>
-                    <!-- /.modal-dialog -->
-                </div>
-                <!-- /.modal -->
-                </section>
-                </form>
-            </div>
-            <!-- /.container -->
-
-        </div>
-        <!-- /.content-wrapper -->
-
-        @include('layouts.partials.footer')
-
-        @include('layouts.partials.queue_sidebar')
-    </div>
-    <!-- ./wrapper -->
+    @include('layouts.partials.queue_sidebar')
+   
 
     <!-- jQuery 3 -->
     @section('scripts')
