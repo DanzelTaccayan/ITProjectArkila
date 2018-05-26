afa00336a98d34ae4de26772b9c202ece060897d
VLORIA2152015 <2152015@slu.edu.ph>
2018-05-26 17:52:42 +0800

    added expire date in UI

--- arkila/resources/views/reservations/showReservation.blade.php
+++ arkila/resources/views/reservations/showReservation.blade.php
@@ -58,6 +58,7 @@
 							<h4>RESERVATION FARE</h4>
 							<div style="border: 1px solid lightgray; margin-bottom: 5%;">
 								<h3 class="text-center" style="padding: 3%; font-size: 40px;"><strong class="text-green">{{$request->fare}}</strong></h3>
+								<p class="text-center"><strong>EXPIRE DATE:</strong></p>
 							</div>
 							<div class="text-center">
 								<button type="submit" name="payment" class="btn btn-success"><i class="fa fa-money"></i> Receive Payment</button>
@@ -68,12 +69,13 @@
 						</strong></h2>
 						<div>
 							<div class="padding-side-10" style="margin-top: 10%;">
+							<p class=""><strong>CAN BE REFUNDED UNTIL:</strong></p>
 							<table class="table table-striped table-bordered">
 								<tbody>
 									<tr>
 										<th>Enter Refund Code</th>
 										<td>
-				   				<input type="text" name="refundCode" class="form-control" required></td>
+				   						<input type="text" name="refundCode" class="form-control" required></td>
 									</tr>
 								</tbody>
 							</table>
