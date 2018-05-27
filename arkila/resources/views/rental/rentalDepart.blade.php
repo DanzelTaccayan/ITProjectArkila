50babe20cfffae0c517292798fe2d4db7b61aaad
VLORIA2152015 <2152015@slu.edu.ph>
2018-05-27 18:12:41 +0800

    added table in computing refund (rental)

--- arkila/resources/views/rental/rentalDepart.blade.php
+++ arkila/resources/views/rental/rentalDepart.blade.php
@@ -1,11 +1,26 @@
 <h2><strong>STATUS: 
 	<span class="text-aqua">PAID</span>
+    <span class="text-gray">CANCELLED</span>
 </strong></h2>
 <div class="">
 	<form action="{{route('rental.updateStatus', $rental->rent_id)}}" method="POST" class="form-horizontal">
     {{ csrf_field() }} {{ method_field('PATCH') }}
         <div class="padding-side-15" style="margin-top: 10%">
-            <h4><strong>REFUNDABLE UNTIL:</strong></h4>
+            <table class="table table-striped table-bordered">
+                <tr>
+                    <th>Paid Fare</th>
+                    <td class="text-right">₱ 200</td>
+                </tr>
+                <tr style="border-bottom: 2px solid black">
+                    <th>Cancellation Fee</th>
+                    <td class="text-right">₱ 100</td>
+                </tr>
+                <tr>
+                    <th>Refund</th>
+                    <td class="text-right"><strong>₱ 100</strong></td>
+                </tr>
+            </table>
+            <p><strong>REFUNDABLE UNTIL:</strong></p>
             <table class="table table-striped table-bordered">
                 <tbody>
                     <tr>
