c1f069387565a87ad3b60a9738e20452d4b3c9e4
Danzel Taccayan <taccayandanzel@gmail.com>
2018-05-27 17:00:12 +0800

    Fixed bugs in memebers

--- arkila/app/Http/Requests/DriverRequest.php
+++ arkila/app/Http/Requests/DriverRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use App\Rules\checkContactNumber;
+use App\Rules\checkOperator;
 use App\Rules\checkSpecialCharacters;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rule;
@@ -26,25 +27,43 @@ class DriverRequest extends FormRequest
      */
     public function rules()
     {
-        return [
-            'profilePicture' => 'bail|nullable|mimes:jpeg,jpg,png|max:3000',
-            'lastName' => ['bail','required','max:25',new checkSpecialCharacters],
-            'firstName' => ['bail','required','max:25',new checkSpecialCharacters],
-            'middleName' => ['bail','nullable','max:25',new checkSpecialCharacters],
-            'contactNumber' => ['bail','max:30','required', new checkContactNumber],
-            'address' => ['bail','required','max:70',new checkSpecialCharacters],
-            'provincialAddress' => ['bail','required','max:70',new checkSpecialCharacters],
-            'gender' => [
-                'bail',
-                'required',
-                Rule::in(['Male', 'Female'])
-            ],
-            'contactPerson' => ['bail','required', 'max:75',new checkSpecialCharacters],
-            'contactPersonAddress' => ['bail','required','max:70',new checkSpecialCharacters],
-            'contactPersonContactNumber' => ['bail','max:30','required', new checkContactNumber],
-            'licenseNo' => 'bail|required',
-            'licenseExpiryDate' => 'bail|required',
-            'sss' => 'nullable'
-        ];
+        if($this->method() == "Post") {
+            return [
+                'operator' => ['nullable',new checkOperator],
+                'profilePicture' => 'bail|nullable|mimes:jpeg,jpg,png|max:3000',
+                'lastName' => ['bail','required','max:25',new checkSpecialCharacters],
+                'firstName' => ['bail','required','max:25',new checkSpecialCharacters],
+                'middleName' => ['bail','nullable','max:25',new checkSpecialCharacters],
+                'contactNumber' => ['bail','max:30','required', new checkContactNumber],
+                'address' => ['bail','required','max:70',new checkSpecialCharacters],
+                'provincialAddress' => ['bail','required','max:70',new checkSpecialCharacters],
+                'gender' => [
+                    'bail',
+                    'required',
+                    Rule::in(['Male', 'Female'])
+                ],
+                'contactPerson' => ['bail','required', 'max:75',new checkSpecialCharacters],
+                'contactPersonAddress' => ['bail','required','max:70',new checkSpecialCharacters],
+                'contactPersonContactNumber' => ['bail','max:30','required', new checkContactNumber],
+                'licenseNo' => 'bail|required',
+                'licenseExpiryDate' => 'bail|required',
+                'sss' => 'nullable'
+            ];
+        } else {
+            return [
+                'operator' => ['nullable',new checkOperator],
+                'profilePicture' => 'bail|nullable|mimes:jpeg,jpg,png|max:3000',
+                'contactNumber' => ['bail','max:30','required', new checkContactNumber],
+                'address' => ['bail','required','max:70',new checkSpecialCharacters],
+                'provincialAddress' => ['bail','required','max:70',new checkSpecialCharacters],
+                'contactPerson' => ['bail','required', 'max:75',new checkSpecialCharacters],
+                'contactPersonAddress' => ['bail','required','max:70',new checkSpecialCharacters],
+                'contactPersonContactNumber' => ['bail','max:30','required', new checkContactNumber],
+                'licenseNo' => 'bail|required',
+                'licenseExpiryDate' => 'bail|required',
+                'sss' => 'nullable'
+            ];
+        }
+
     }
 }
