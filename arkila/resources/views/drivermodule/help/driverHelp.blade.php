9c62040a73b6d72695185094abecd13ecf7a2fda
C. Deric <c.dalayoan98@gmail.com>
2018-06-05 01:31:12 +0800

    Edited help page driver module

--- arkila/resources/views/drivermodule/help/driverHelp.blade.php
+++ arkila/resources/views/drivermodule/help/driverHelp.blade.php
@@ -11,13 +11,10 @@
             <div class="box-body">
                 <ul>
                     <li>
-                        <strong>Step 1:</strong> Go to the "Home" tab on the navigation bar.
+                       If there are any announcements made by the admin, it will be shown below at the "Announcements" tab.
                     </li>
                     <li>
-                        <strong>Step 2:</strong> View announcements in the "Announcements" tab.
-                    </li>
-                    <li>
-                        <strong>Step 3:</strong> View van's queue number in the "On Queue" tab.
+                    	If there are any associated driver with a van, it will be shown on the upper section of the page. And if the van is on queue, the van's queue number will also be shown on the upper section of the page.
                     </li>
                 </ul>
             </div>
@@ -66,6 +63,27 @@
             </div>
             <!-- box-body-->
         </div>
+
+             <div class="box box-solid">
+            <div class="box-header  with-border">
+                <h5 class="text-center"><strong>Rentals</strong></h5>
+            </div>
+            <div class="box-body">
+                <ul>
+                    <li>
+                        <strong>Step 1:</strong> Go to the "Rentals" tab on the navigation bar.
+                    </li>
+                    <li>
+                        <strong>Step 2:</strong> After redirecting to the rentals page, you can see the list of rental requests wherein you can accept or decline a request.
+                    </li>
+                    <li>
+                        <strong>Step 3:</strong> You can also see the list of rentals that you accepted by clicking the "History" button.
+                    </li>
+                </ul>
+            </div>
+            <!-- box-body-->
+        </div>
+
         <!-- box-->
         <div class="box box-solid">
             <div class="box-header with-border">
