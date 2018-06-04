ded794d7fd414dde1b120fb4eb8f4c2f48e43a57
VLORIA2152015 <2152015@slu.edu.ph>
2018-06-04 17:25:35 +0800

    addwed sign out icon in headers of admin in signout button

--- arkila/resources/views/layouts/partials/header.blade.php
+++ arkila/resources/views/layouts/partials/header.blade.php
@@ -89,7 +89,7 @@
                            </a>
                           </div>
                           <div class="pull-right">
-                             <a  href="{{route('logout')}}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();" class="btn btn-default">Sign out</a>
+                             <a  href="{{route('logout')}}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();" class="btn btn-default"><i class="fa fa-sign-out"></i> Sign out</a>
                             <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                                 {{csrf_field()}}
                             </form>
