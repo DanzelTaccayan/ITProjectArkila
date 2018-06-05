cb4f29594664817f8b7ff2152e2f2d57114867ab
Danzel Taccayan <taccayandanzel@gmail.com>
2018-06-06 07:51:39 +0800

    fixed restore dbase

--- arkila/app/Http/Controllers/RestoreDatabaseController.php
+++ arkila/app/Http/Controllers/RestoreDatabaseController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 use BackupManager\Manager;
+use DB;
 
 class RestoreDatabaseController extends Controller{
     protected $manager;
@@ -12,8 +13,18 @@ class RestoreDatabaseController extends Controller{
     }
 
     public function restoreDatabase() {
-        $this->manager->makeRestore()->run('local', '/database-backup/arkilaBackup.gz', 'mysql', 'gzip');
 
-        return back()->with('success','Successfully Restored Database Backup');
+        DB::beginTransaction();
+        try{
+            $this->manager->makeRestore()->run('local', '/database-backup/arkilaBackup.gz', 'mysql', 'gzip');
+
+            DB::commit();
+            return back()->with('success','Successfully Restored Database Backup');
+        }
+        catch(\Exception $e) {
+            DB::rollback();
+            return back()->withErrors('Oops! Something went wrong on the server. If the problem persists contact the administrator');
+        }
+
     }
 }
