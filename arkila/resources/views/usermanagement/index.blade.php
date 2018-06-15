bbab6979b875db79b8fdcf60eca25646c281e237
Juan Miguel Delos Santos <2153820@slu.edu.ph>
2018-06-15 15:41:44 +0800

    remove driver

--- arkila/resources/views/usermanagement/index.blade.php
+++ arkila/resources/views/usermanagement/index.blade.php
@@ -12,86 +12,36 @@
         <h2 class="text-white">USER MANAGEMENT</h2>
     </div>
     <div class="box">
-        <div class="col-xl-6">
-            <!-- Custom Tabs -->
-            <div class="nav-tabs-custom">
-                <ul class="nav nav-tabs">
-                    <li class="active"><a href="#driverUser" data-toggle="tab">DRIVER</a></li>
-                    <li><a href="#customerUser" data-toggle="tab">CUSTOMER</a></li>
-                </ul>
-
-                <div class="tab-content">
-                    <!-- /.tab-pane -->
-                    <div class="tab-pane active" id="driverUser">
-
-
-                        <div class="box-body">
-                            <div class="table-responsive">
-                            <table class="table table-bordered table-striped dataTable">
-                                <thead>
-                                    <tr>
-                                        <th>Name</th>
-                                        <th>Username</th>
-                                        <th class="text-center">Action</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <!-- driver -->
-                                    @foreach($userDrivers as $userDriver)
-                                    <tr>
-                                        <td>{{strtoupper($userDriver->last_name)}}, {{strtoupper($userDriver->first_name)}}</td>
-                                        <td>{{$userDriver->username}}</td>
-                                        <td class="center-block">
-                                            <div class="text-center">
-                                                <a href="/home/user-management/driver/{{$userDriver->id}}" class="btn btn-primary btn-sm"><i class="glyphicon glyphicon-cog"></i>MANAGE ACCOUNTS</a>
-                                            </div>
-                                        </td>
-                                    </tr>
-                                    @endforeach
-                                </tbody>
-                            </table>
-                            </div>
-                        </div>
-                    </div>
-
-
-                    <div class="tab-pane" id="customerUser">
-                        <div class="box-body">
-                            <div class="table-responsive">
-                            <table class="table table-bordered table-striped dataTable">
-                                <thead>
-                                    <tr>
-                                        <th>Name</th>
-                                        <th>Username</th>
-                                        <th>Email Address</th>
-                                        <th>Action</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <!-- customer -->
-                                    @foreach($userCustomers as $userCustomer)
-                                    <tr>
-                                        <td>{{strtoupper($userCustomer->last_name)}}, {{strtoupper($userCustomer->first_name)}}</td>
-                                        <td>{{$userCustomer->username}}</td>
-                                        <td>{{$userCustomer->email}}</td>
-                                        <td class="center-block">
-                                            <div class="text-center">
-                                                <a href="/home/user-management/customer/{{$userCustomer->id}}" class="btn btn-primary btn-sm"><i class="glyphicon glyphicon-cog"></i>MANAGE ACCOUNTS</a>
-                                            </div>
-                                        </td>
-                                    </tr>
-                                    @endforeach
-                                </tbody>
-                            </table>
-                            </div>
-                        </div>
-                    </div>
-
-
-                    <!-- /.box-body -->
+        <div class="col-xl-6">    
+            <div class="box-body">
+                <div class="table-responsive">
+                <table class="table table-bordered table-striped dataTable">
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Username</th>
+                            <th>Email Address</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <!-- customer -->
+                        @foreach($userCustomers as $userCustomer)
+                        <tr>
+                            <td>{{strtoupper($userCustomer->last_name)}}, {{strtoupper($userCustomer->first_name)}}</td>
+                            <td>{{$userCustomer->username}}</td>
+                            <td>{{$userCustomer->email}}</td>
+                            <td class="center-block">
+                                <div class="text-center">
+                                    <a href="/home/user-management/customer/{{$userCustomer->id}}" class="btn btn-primary btn-sm"><i class="glyphicon glyphicon-cog"></i>MANAGE ACCOUNTS</a>
+                                </div>
+                            </td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
                 </div>
-            </div>
-            <!-- /.tab-pane -->
+            </div>                 
         </div>
     </div>
 </div>
